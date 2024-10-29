26aa03c37ef6ae20494dcfdeada4b86db84d03fa
kangddong <rkdehddud96@naver.com>
2024-10-30 00:00:34 +0900

    2. 뷰를 구성하십시오

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -10,12 +10,18 @@ import SwiftUI
 struct MeetingView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ProgressView(value: 10, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
         }
-        .padding()
     }
 }
 
