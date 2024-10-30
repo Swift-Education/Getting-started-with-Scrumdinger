a8596b1395c9f0aec028b83136ce08bac94772ff
kangddong <rkdehddud96@naver.com>
2024-10-30 11:50:57 +0900

    3. 카드 뷰 만들기

---
+++ Scrumdinger/Views/CardView.swift
@@ -0,0 +1,31 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by 강동영 on 10/30/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+            }
+        }
+        
+    }
+}
+
+#Preview(traits: .fixedLayout(width: 400, height: 60)) {
+    let scrum = DailyScrum.sampleData[0]
+    CardView(scrum: scrum)
+        .background(scrum.theme.mainColor)
+}
