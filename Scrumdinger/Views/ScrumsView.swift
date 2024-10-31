96cd21c7625cc4cf98edc3f76078a23fa604be06
kangddong <rkdehddud96@naver.com>
2024-10-31 17:28:08 +0900

    1. 일일 스크럼 목록 표시

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by 강동영 on 10/30/24.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
