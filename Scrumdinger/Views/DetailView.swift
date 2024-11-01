ab1aaf96480823a5eba9efe5aec6ab6885e77111
kangddong <rkdehddud96@naver.com>
2024-11-01 10:37:48 +0900

    2. 세부 뷰 만들기

---
+++ Scrumdinger/Views/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by 강동영 on 11/1/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
