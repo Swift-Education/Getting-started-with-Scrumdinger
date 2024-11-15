4638e90d6c1fc7e3f25ac08d26e638bf50fcb6fc
kangddong <rkdehddud96@naver.com>
2024-11-15 22:25:14 +0900

    1. 편집 뷰를 사용하여 새로운 스크럼을 만드세요

---
+++ Scrumdinger/Views/NewScrumSheet.swift
@@ -0,0 +1,41 @@
+//
+//  NewScrumSheet.swift
+//  Scrumdinger
+//
+//  Created by 강동영 on 11/14/24.
+//
+
+import SwiftUI
+
+struct NewScrumSheet: View {
+    @State private var newScrum = DailyScrum.emptyScrum
+    @Binding var scrums: [DailyScrum]
+    @Binding var isPresentingNewScrumView: Bool
+    
+    var body: some View {
+        NavigationStack {
+            DetailEditView(scrum: $newScrum)
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button("Dismiss") {
+                            isPresentingNewScrumView = false
+                        }
+                    }
+                    ToolbarItem(placement: .confirmationAction) {
+                        Button("Add") {
+                            scrums.append(newScrum)
+                            isPresentingNewScrumView = false
+                        }
+                    }
+                }
+        }
+        
+    }
+}
+
+#Preview {
+    NewScrumSheet(
+        scrums: .constant(DailyScrum.sampleData),
+        isPresentingNewScrumView: .constant(true)
+    )
+}
